ce2e741f65e1b00da9bf928811b98b41951c64aa
Katarzyna Elliott-Maksymowicz <maksymowicz.katarzyna@gmail.com>
2018-11-11 21:54:25 -0500

    beautifies bookshelf

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,43 +1,63 @@
-import React, {Component} from "react";
+import React, {
+    Component
+}
+from "react";
 import BooksGrid from "./BooksGrid";
 import PropTypes from 'prop-types'
 
 export default class Bookshelf extends Component {
 
-  static propTypes = {
-      books: PropTypes.array.isRequired,
-      updateShelf: PropTypes.func.isRequired
-  }
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        updateShelf: PropTypes.func.isRequired
+    }
 
-  state = {
-    shelves: [              //array with shelf names
-        {value: 'currentlyReading', name: 'Now Reading' },
-        {value: 'wantToRead',  name: 'Want to Read'},
-        {value: 'read', name: 'Already Read'}
-    ]
-}
+    state = {
+        shelves: [ //array with shelf names
+            {
+                value: 'currentlyReading',
+                name: 'Now Reading'
+            }, {
+                value: 'wantToRead',
+                name: 'Want to Read'
+            }, {
+                value: 'read',
+                name: 'Already Read'
+            }
+        ]
+    }
 
-  render(){
-    const { books, updateShelf } = this.props
-    const { shelves } = this.state
+    render() {
+        const {
+            books, updateShelf
+        } = this.props
+        const {
+            shelves
+        } = this.state
 
-    return (
-      <div>
-            {shelves.map((shelf, idx) => (
+        return ( < div > {
+            shelves.map((shelf, idx) => (
                 //use shelves state current array index as individual key
-                <div key={idx} className="bookshelf">
-                    <h2 className="bookshelf-title">{shelf.name}</h2>
-                    <div className="bookshelf-books">
-                        <BooksGrid
-                            //filter the books state by matching shelf with current shelves state array
-                            shelfBooks={books.filter(book => book.shelf === shelf.value)}
-                            books = {books}
-                            updateShelf={updateShelf}
-                        />
-                    </div>
-                </div>
-            ))}
-           </div>
-        )
+                < div key = {
+                    idx
+                }
+                className = "bookshelf" >
+                < h2 className = "bookshelf-title" > {
+                    shelf.name
+                } < /h2> < div className = "bookshelf-books" >
+                < BooksGrid
+                //filter the books state by matching shelf with current shelves state array
+                shelfBooks = {
+                    books.filter(book => book.shelf === shelf.value)
+                }
+                books = {
+                    books
+                }
+                updateShelf = {
+                    updateShelf
+                }
+                /> < /div> < /div>
+            ))
+        } < /div>)
     }
 }
